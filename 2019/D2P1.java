79ba77621fa3b934723c91f1303918199629c7b6
Jason Chan <jchan61@stuy.edu>
2025-02-06 14:37:33 -0500

    completed day 2 part 1

--- 2019/D2P1.java
+++ 2019/D2P1.java
@@ -15,23 +15,25 @@ public class D2P1{
         intCode.add(Integer.parseInt(i));
       }
       int i = 0;
-      //intCode.set(0, 12);
-      //intCode.set(1, 2);
+      intCode.set(1, 12);
+      intCode.set(2, 2);
 
       while (intCode.get(i) != 99){
-        if (intCode.get(i) == 1){
-          int first = intCode.get(i + 1), second = intCode.get(i + 2), change = intCode.get(i + 3);
-          intCode.set(change, first + second);
-        }
-        else if (intCode.get(i) == 2){
-          int first = intCode.get(i + 1), second = intCode.get(i + 2), change = intCode.get(i + 3);
-          intCode.set(change, first * second);
+        if (intCode.get(i) == 1 || intCode.get(i) == 2){
+          int first = intCode.get(intCode.get(i + 1)), second = intCode.get(intCode.get(i + 2)), change = intCode.get(i + 3);
+          if (intCode.get(i) == 1){
+            intCode.set(change, first + second);
+          }
+          else{
+            intCode.set(change, first * second);
+          }
+          i += 3;
         }
         i++;
       }
 
       data.close();
-      System.out.println(intCode);
+      //System.out.println(intCode);
       return intCode.get(0);
     } catch (FileNotFoundException ex) {
       System.out.println("File not found");
